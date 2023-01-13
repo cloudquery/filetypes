9f632daa98c1a1d177580725b053f2c1222a3e33
bbernays <ben@bernays.me>
2023-01-13 09:56:09 -0500

    Update write.go

--- csv/write.go
+++ csv/write.go
@@ -7,8 +7,17 @@ import (
 	"github.com/cloudquery/plugin-sdk/schema"
 )
 
-func WriteTableBatch(w io.Writer, _ *schema.Table, resources [][]any) error {
+func WriteTableBatch(w io.Writer, table *schema.Table, resources [][]any, headers bool) error {
 	writer := csv.NewWriter(w)
+	if headers {
+		tableHeaders := make([]string, len(table.Columns))
+		for index, header := range table.Columns {
+			tableHeaders[index] = header.Name
+		}
+		if err := writer.Write(tableHeaders); err != nil {
+			return err
+		}
+	}
 	for _, resource := range resources {
 		record := make([]string, len(resource))
 		for i, v := range resource {
