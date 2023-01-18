7c4ca6be40b1d3b909944c38ad25ed194b598875
bbernays <ben@bernays.me>
2023-01-18 07:53:31 -0600

    Update read.go

--- csv/read.go
+++ csv/read.go
@@ -9,7 +9,7 @@ import (
 	"github.com/cloudquery/plugin-sdk/schema"
 )
 
-func (cl *Client) Read(r io.Reader, table *schema.Table, sourceName string, res chan<- []any) error {
+func (cl *Client) Read(r io.Reader, table *schema.Table, _ string, res chan<- []any) error {
 	reader := csv.NewReader(r)
 	reader.Comma = cl.Delimiter
 	sourceNameIndex := table.Columns.Index(schema.CqSourceNameColumn.Name)
