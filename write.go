35173eefd8eef09dd58160ed8058e7f79227a9d3
bbernays <ben@bernays.me>
2023-01-20 09:28:11 -0600

    Update write.go

--- write.go
+++ write.go
@@ -6,7 +6,7 @@ import (
 	"github.com/cloudquery/plugin-sdk/schema"
 )
 
-func (cl *Client) WriteTableBatch(w io.Writer, table *schema.Table, resources [][]any) error {
+func (cl *Client) WriteTableBatchFile(w io.Writer, table *schema.Table, resources [][]any) error {
 	switch cl.spec.Format {
 	case FormatTypeCSV:
 		if err := cl.csv.WriteTableBatch(w, table, resources); err != nil {
