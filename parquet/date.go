96268690c38fd51d28fd9e211cf67d80c18f2202
Alex Shcherbakov <candiduslynx@users.noreply.github.com>
2024-05-06 18:53:34 +0300

    fix(deps): Upgrade `github.com/apache/arrow/go` to `v16` (#494)

--- parquet/date.go
+++ parquet/date.go
@@ -1,9 +1,9 @@
 package parquet
 
 import (
-	"github.com/apache/arrow/go/v15/arrow"
-	"github.com/apache/arrow/go/v15/arrow/array"
-	"github.com/apache/arrow/go/v15/arrow/memory"
+	"github.com/apache/arrow/go/v16/arrow"
+	"github.com/apache/arrow/go/v16/arrow/array"
+	"github.com/apache/arrow/go/v16/arrow/memory"
 )
 
 func reverseTransformDate64(arr *array.Date32) *array.Date64 {
--- write.go
+++ write.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/apache/arrow/go/v15/arrow"
+	"github.com/apache/arrow/go/v16/arrow"
 	"github.com/cloudquery/filetypes/v4/types"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 )
